b7d8e234e2afcf97a94dc95862df852cf9bad040
Lorenzo Rybu Buglione <lorenzorybuglione@gmail.com>
2023-09-21 18:30:19 -0300

    Header with Search Bar

--- src/componentes/Cabecalho/index.jsx
+++ src/componentes/Cabecalho/index.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import CampoDeTexto from "../CampoDeTexto";
 
 const HeaderStyled = styled.header`
   padding: 60px 0;
@@ -9,6 +10,7 @@ const HeaderStyled = styled.header`
 const Cabecalho = () => {
   return <HeaderStyled>
     <img src="/imagens/logo.png" alt="Logo do Space App"/>
+    <CampoDeTexto/>
   </HeaderStyled>;
 };
 
---
+++ src/componentes/CampoDeTexto/index.jsx
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+import search from "/icones/search.png"
+
+const ContainerStyled = styled.div`
+    position: relative;
+    display: inline-block;
+`;
+
+const TextInputStyled = styled.input`
+  color: #D9D9D9;
+  font-size: 20px;
+  font-weight: 400;
+  line-height: 20px;
+  width: 566;
+  height: 56px;
+  padding: 12px 16px;
+  border-radius: 10px;
+  border: 2px solid;
+  background: transparent;
+  border-color: #C98CF1;
+`;
+
+const IconStyled = styled.img`
+    position: absolute;
+    top: 20px;
+    right: 10px;
+    width: 38px;
+    height: 38px;
+`
+
+const CampoDeTexto = () => {
+  return (
+  <ContainerStyled>
+
+  <TextInputStyled placeholder="O que você procura?"/>
+    <IconStyled src={search} alt="ícone de Lupa"/>
+  </ContainerStyled>)
+};
+
+export default CampoDeTexto;
